a6acd8558860791b19e0d3be51606696c41a32b9
luizhenriquea2002-cell <luizhenriquea2002@gmail.com>
2026-04-01 23:45:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ function comprar(jogo) {
 function ir(jogo){
   window.location.href = `produto.html?jogo=${jogo}`;
 }
+let produtoAtual = "";
+let precoAtual = "";
+
+function abrirModal(nome, preco){
+  produtoAtual = nome;
+  precoAtual = preco;
+
+  document.getElementById("produtoNome").innerText = nome;
+  document.getElementById("produtoPreco").innerText = "R$ " + preco;
+
+  document.getElementById("modal").style.display = "flex";
+}
+
+function fecharModal(){
+  document.getElementById("modal").style.display = "none";
+}
+
+function finalizarCompra(){
+  const msg = `Quero comprar ${produtoAtual} por R$${precoAtual}`;
+  window.open("https://t.me/Lz7vendas?text=" + encodeURIComponent(msg));
+}
